01f3175895afd865cb405b4896c23879236ea82c
godgold11@gmail.com <godgold11@gmail.com>
2023-07-02 03:45:12 +0300

    Add project files.

---
+++ CodeChallenge06/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeChallenge06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
